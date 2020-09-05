e819805684020e0be567e78edd5def6d6ed70993
硅谷彭于晏 <NB>
2020-09-05 11:41:52 +0800

    git测试练习

--- src/main/java/at/guigu/dao/user.java
+++ src/main/java/at/guigu/dao/user.java
@@ -4,7 +4,7 @@ public class user {
     public void usertext(){
         String user = "xiaomingsss";
         String user1 = "xiaomingsss1";
-        String user = "xiaoming";
+        String user3 = "xiaoming";
         String user2 = "xiaoming222";
         System.out.println(user);
     }
